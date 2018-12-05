283569ff5a0c3ccc1afa109710c2716f142149c2
Nuno Ventura <ventura_87@outlook.com>
2018-12-05 23:11:25 +0000

    js files

---
+++ supportFunction.js
@@ -0,0 +1,16 @@
+﻿
+        function send() {
+
+            var input1, input2, input3;
+
+            input1 = input = document.getElementById('nome');
+            input2 = input = document.getElementById('email');
+            input3 = input = document.getElementById('mensagem');
+
+            input1.value = '';
+            input2.value = '';
+            input3.value = '';
+
+            alert("E-mail sent.");
+
+        }
