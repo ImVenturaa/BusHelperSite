ded1de9aeda4017f09779719dcd41487bf05d45d
Nuno Ventura <nuno_ventura98@hotmail.com>
2018-12-06 11:02:26 +0000

    support validations

--- supportFunction.js
+++ supportFunction.js
@@ -7,6 +7,26 @@
             input2 = input = document.getElementById('email');
             input3 = input = document.getElementById('mensagem');
 
+            if(input1.value == ''){
+                alert("Please fill every field.");
+                return;
+            }
+            if(input2.value == ''){
+                alert("Please fill every field.");
+                return;
+            }
+            if(input3.value == ''){
+                alert("Please fill every field.");
+                return;
+            }
+
+            var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+            if (!filter.test(input2.value)) {
+                alert('Please provide a valid email address');
+                input2.focus();
+                return;
+            }
             input1.value = '';
             input2.value = '';
             input3.value = '';
